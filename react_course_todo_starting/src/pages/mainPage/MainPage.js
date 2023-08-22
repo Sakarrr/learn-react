458f1264d713bfeb14cc6e3fdb481e0b727fc5b7
Sakarrr <ctha.sakar@gmail.com>
2023-08-22 20:19:25 +0545

    dyno-academy/react-learning-45

--- react_course_todo_starting/src/pages/mainPage/MainPage.js
+++ react_course_todo_starting/src/pages/mainPage/MainPage.js
@@ -1,7 +1,23 @@
 import { Link } from "react-router-dom";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
+
 const MainPage = () => {
+
+     const history = useHistory();
+
+     const login = () => {
+          // Logic....
+
+          // Success...
+          // history.push("/add");
+
+          history.replace("/add");
+     }
      return <> This is Main Page
      <Link to="/add">Go To Add Page </Link>
+
+
+     <button onClick={ login }> Login </button>
      </>
 }
 
